705ffe4c3a8d5484cc379560fe2b4c430831da47
Aleksandra Żyłka <261849@student.pwr.edu.pl>
2024-11-24 20:06:40 +0100

    Dunkcjonalnosc dodawania i modyfikowania treningu

--- src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingController.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingController.java
@@ -1,11 +1,15 @@
 package com.capgemini.wsb.fitnesstracker.training.internal;
 
+import com.capgemini.wsb.fitnesstracker.training.api.Training;
+import com.capgemini.wsb.fitnesstracker.training.api.TrainingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/trainings")
@@ -13,6 +17,8 @@ import java.util.List;
 class TrainingController {
     private final TrainingServiceImpl trainingService;
     private final TrainingMapper trainingMapper;
+
+    private final TrainingService trainingServiceAPI;
 
     @GetMapping
     public List<TrainingDto> getAllTrainings() {
@@ -45,4 +51,26 @@ class TrainingController {
                 .map(trainingMapper::toDto).
                 toList();
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TrainingDto addNewTraining(@RequestBody TrainingDto training) {
+        Training trainingEntity = trainingMapper.toEntity(training);
+        Training savedTraining = trainingServiceAPI.createTraining(trainingEntity);
+        return trainingMapper.toDto(savedTraining);
+    }
+
+
+    @PutMapping("/{id}")
+    public TrainingDto updateTraining(@PathVariable Long id, @RequestBody TrainingDto trainingDto) {
+        Training correctedTraining = trainingServiceAPI.updateTraining(id, trainingMapper.toEntity(trainingDto));
+        return trainingMapper.toDto(correctedTraining);
+    }
+
+    @PatchMapping("/{id}")
+    public TrainingDto partiallyUpdateTraining(@PathVariable Long id, @RequestBody Map<String, Object> update) {
+        Training correctedTraining = trainingServiceAPI.partiallyUpdateTraining(id, update);
+        return trainingMapper.toDto(correctedTraining);
+    }
+
 }
